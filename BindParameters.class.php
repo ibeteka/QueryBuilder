729c972d34a5e23b3dd39d262df914b38913991e
ibeteka <brahmsos@gmail.com>
2015-06-29 12:21:56 +0200

    Create BindParameters.class.php

---
+++ BindParameters.class.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace lib\QueryBuilder;
+
+
+
+/**
+ * 
+ * @author xan_tounkara
+ *
+ */
+class BindParameters {
+	
+	private $placeholders;
+	private $value;
+	private $type;
+	
+	
+	
+	public function getPlaceholders() {
+		return $this->placeholders;
+	}
+	public function setPlaceholders($placeholders) {
+		$this->placeholders = $placeholders;
+		return $this;
+	}
+	public function getValue() {
+		return $this->value;
+	}
+	public function setValue($value) {
+		$this->value = $value;
+		return $this;
+	}
+	public function getType() {
+		return $this->type;
+	}
+	public function setType($type) {
+		$this->type = $type;
+		return $this;
+	}
+	
+	
+	
+	public function __construct($p,$v,$t){
+		
+		$this->placeholders = $p;
+		$this->value 		= $v;
+		$this->type			= $t;
+	}
+	
+}
